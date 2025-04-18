c658e65ef454db0d3bc4f0c18cd8ca2c0fac513b
Movindu Liyanage <142730691+Movin21@users.noreply.github.com>
2025-04-18 18:58:43 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'SESC T-shirt Tracking System',
+  description: '24/25 SESC T-shirt Tracking System',
+  generator: 'movin.dev',
 }
 
 export default function RootLayout({
